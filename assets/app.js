f70156a10f7cf0a1018b077c0bf2813013a1af8b
zulmi48 <zulmi.f48@gmail.com>
2022-09-05 23:37:29 +0700

    Add javascript

---
+++ assets/app.js
@@ -0,0 +1,23 @@
+document.querySelector("#btn-list").onclick = function () {
+	if (document.querySelector("#input-list").value.length == 0) {
+		alert("Plese enter your list...");
+	} else {
+		document.querySelector("#todo-list").innerHTML += `
+         <ul class="list-group">
+           <li class="list-group-item">
+             - ${document.querySelector("#input-list").value}
+             <button class="delete btn btn-sm btn-outline-danger mx-2">
+               <i class="bi bi-trash3"></i>
+             </button>
+           </li>
+         </ul>
+         `;
+		var currentTask = document.querySelectorAll(".delete");
+		for (var i = 0; i < currentTask.length; i++) {
+			currentTask[i].onclick = function () {
+				this.parentNode.remove();
+			};
+		}
+	}
+	document.querySelector("#input-list").value = "";
+};
